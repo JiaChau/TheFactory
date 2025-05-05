4dc6d4095e1a6b121481c599d9cd4a0aa0bdbb17
Yubeen971 <92639469+Yubeen971@users.noreply.github.com>
2025-05-05 18:32:58 -0400

    NevMesh (Test)
    - Test of the NevMesh

---
+++ Assets/Scenes/Testing AI/TestAI.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.AI;
+
+
+public class TestAI : MonoBehaviour
+{
+
+ public Transform[] waypoints;
+
+ private NavMeshAgent priestAgent;
+
+ private float normalSpeed = 30f;
+
+ private int waypointIndex;
+
+ private Vector3 target;
+
+
+
+
+    // Update is called once per frame
+void Start()
+{
+    priestAgent = GetComponent<NavMeshAgent>();
+    UpdateDestination();
+}
+
+void Update()
+
+{
+
+
+
+            //We want to continue patrol
+
+Patrol();
+
+
+}
+
+
+
+    //This will set the destination of the navmesh agent to current waypoint index
+
+void UpdateDestination()
+
+{
+
+target = waypoints[waypointIndex].position;
+
+priestAgent.SetDestination(target);
+
+
+
+}
+
+
+    //This increases the index, taking into account the size of the array and setting it to "0" if its equal to it
+
+void IterateWaypointIndex()
+{
+
+++waypointIndex;
+
+if (waypointIndex == waypoints.Length)
+{
+    waypointIndex = 0;
+    Debug.Log("Test2");
+}
+
+Debug.Log(waypointIndex);
+}
+
+
+
+    //Stops scary music, resets slower speed, returns to slow walking animation
+
+private void Patrol()
+
+{
+
+priestAgent.speed = normalSpeed;
+
+        //Also, handles the waypoints of the arrays/patrol points
+
+if (Vector3.Distance(transform.position, target) < 5)
+
+{
+
+IterateWaypointIndex();
+
+UpdateDestination();
+
+}
+
+}
+
+}
